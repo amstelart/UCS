8761dca09740e78156c50d70fe762bb76ae1a54b
amstelart <amsteldroid@gmail.com>
2019-08-07 20:31:09 +0300

    add related-products

--- src/js/global-script.js
+++ src/js/global-script.js
@@ -20,6 +20,41 @@ $( document ).ready(function() {
     count:                      true
   });
 
+  $('#product-related-slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: !0,
+    dots: !1,
+    responsive: [
+      {
+        breakpoint: 1300,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 724,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+
   $("#product-summary-slider").slick({
       mobileFirst: !0,
       infinite: !0,
